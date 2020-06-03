30646149cce3a4143205a7de49e678c469bd42f0
Hans ter Horst <hbhorst@mac.com>
2020-06-03 08:04:08 +0200

    add an fetchSearchRequest()

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+const API_KEY = '68dd7cc2';
+const OMDB_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
 
 export default class App extends Component {
+	state = {
+		isLoading: false,
+		movie: [],
+	};
+
+	fetchSearchRequest = async (title) => {
+		const searchRequest = OMDB_URL + `t=${title}`;
+		this.setState({ isLoading: true });
+		const movieData = await fetch(searchRequest).then((res) => res.json());
+
+		this.setState({
+			movie: movieData,
+			isLoading: false,
+		});
+
+		console.log(this.state.movie);
+	};
+
+	async componentDidMount() {
+		console.log('componentDidMount');
+		this.fetchSearchRequest('star wars');
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Hello React</h1>
+				<h2>{this.state.isLoading ? 'Loading...' : null}</h2>
 			</div>
 		);
 	}
