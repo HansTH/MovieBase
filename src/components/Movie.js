5feb9fafb9c1c166104ee161eea58a792e03701c
Hans ter Horst <hbhorst@mac.com>
2020-06-03 14:04:48 +0200

    add error checking in the Movie component and add Page404 component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import Page404 from './Page404';
+import image404 from '../assets/image-not-found.png';
 
 function Movie(props) {
-	const { Title, Poster, Year, Plot } = props.movie;
-	return (
-		<div>
-			<img src={Poster} alt='Movie Poster' />
-			<h2>{Title}</h2>
-			<h4>{Year}</h4>
-			<p>{Plot}</p>
-		</div>
-	);
+	const { Title, Poster, Year, Plot, Response, Error } = props.movie;
+
+	const poster = Poster !== 'N/A' ? Poster : image404;
+	const plot = Plot !== 'N/A' ? Plot : 'Geen beschijving aanwezig';
+
+	if (Response === 'True') {
+		return (
+			<div>
+				<img src={poster} alt='Movie Poster' />
+				<h2>{Title}</h2>
+				<h4>{Year}</h4>
+				<p>{plot}</p>
+			</div>
+		);
+	} else {
+		return <Page404 message={Error} />;
+	}
 }
 
 export default Movie;
