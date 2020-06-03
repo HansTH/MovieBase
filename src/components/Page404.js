62fae878d32a4703677c0d8089f3e00511ad7186
Hans ter Horst <hbhorst@mac.com>
2020-06-03 19:35:03 +0200

    refactor to display multiple messages

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -2,12 +2,21 @@ import React from 'react';
 import image404 from '../assets/image-not-found.png';
 
 const Page404 = ({ message }) => {
-	return (
-		<div>
-			<img src={image404} alt='Image not found' />
-			<h2>{message}</h2>
-		</div>
-	);
+	console.log(message);
+	if (message === 'Something went wrong.' || message === undefined) {
+		return (
+			<div>
+				<h2>{message ? message : 'Sorry Page Not Found!'}</h2>
+			</div>
+		);
+	} else {
+		return (
+			<div>
+				<img src={image404} alt='Image not found' />
+				<h2>{message}</h2>
+			</div>
+		);
+	}
 };
 
 export default Page404;
